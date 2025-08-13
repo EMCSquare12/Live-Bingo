926fbeb2a6a47d3c282c690abe44ceec8aa7f4d4
EMCSquare12 <ninelcaalim12@gmail.com>
2025-08-13 17:26:27 +0800

    changes in server

--- live-bingo/src/server/server.js
+++ live-bingo/src/server/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -11,16 +12,30 @@ const io = new Server(server, {
 
 app.use(cors());
 const games = {};
+
 io.on("connection", (socket) => {
   console.log("Client connected");
 
   // Create room
-  socket.on("create-room", (hostName, cardNumber) => {
+  socket.on("create-room", (hostName, cardNumber, cardWinningPattern) => {
     const roomCode = uuidv4().replace(/-/g, "").substring(0, 6).toUpperCase();
-    console.log("Host Name :", hostName);
-    socket.emit("room-created", roomCode);
-    games[roomCode] = {hostName, cardNumber}
-    console.log(games);
+    games[roomCode] = { hostName, cardNumber, cardWinningPattern };
+
+    socket.join(roomCode); // make socket part of that room
+    socket.emit("room-created", roomCode, games[roomCode].hostName);
+
+    console.log(`Room created: ${roomCode}`, games);
+  });
+
+  // Rejoin existing room
+  socket.on("rejoin-room", (roomCode) => {
+    if (games[roomCode]) {
+      socket.join(roomCode);
+      socket.emit("room-data", games[roomCode]); // send game state back
+      console.log(`Client rejoined room ${roomCode}`);
+    } else {
+      socket.emit("error", "Room does not exist");
+    }
   });
 });
 
