3752342f6cb12925553dafaeeb42702ad94ebdf3
EMCSquare12 <ninelcaalim12@gmail.com>
2025-09-15 16:57:37 +0800

    Feat: added env file

--- live-bingo/src/utils/socket.js
+++ live-bingo/src/utils/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Create one socket instance for the whole app
-export const socket = io("http://localhost:3001", {
+export const socket = io(import.meta.env.VITE_SOCKET_URL, {
     autoConnect: true,
     reconnection: true,
     reconnectionAttempts: 10,
