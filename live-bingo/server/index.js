e57fe377bb1d29eb725e1858be243b37ca470f67
EMCSquare12 <ninelcaalim12@gmail.com>
2025-09-17 15:20:15 +0800

    Fix: Server React Build

--- live-bingo/server/index.js
+++ live-bingo/server/index.js
@@ -13,10 +13,10 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(cors());
 
 // Serve React build
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "..", "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
 });
 
 // Register socket events
